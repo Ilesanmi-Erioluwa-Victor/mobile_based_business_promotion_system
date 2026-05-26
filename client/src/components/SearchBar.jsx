ade46137f57b1fa559d7d3a581cf149e54b3d5fd
Ilesanmi Erioluwa Victor <ilesanmierioluwavictor@gmail.com>
2026-05-26 04:18:53 +0100

    done fixing

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -12,7 +12,7 @@ const SearchBar = ({ initialQuery = '' }) => {
   };
 
   return (
-    <form onSubmit={submit} className="flex flex-col gap-3 rounded-2xl border border-border bg-white p-2 sm:flex-row">
+    <form onSubmit={submit} className="flex flex-col gap-3 rounded-2xl border border-border bg-white p-2 sm:flex-row text-black">
       <div className="relative flex-1">
         <Search className="absolute left-4 top-1/2 -translate-y-1/2 text-muted" size={18} />
         <input className="h-12 w-full rounded-xl border-0 bg-transparent pl-11 pr-4 text-sm outline-none" value={q} onChange={(event) => setQ(event.target.value)} placeholder="Search businesses, categories or services" />
